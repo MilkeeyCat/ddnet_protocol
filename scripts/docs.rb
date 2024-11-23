88454cf3b697c51cab32586ef95dbe2192b136f1
ChillerDragon <chillerdragon@gmail.com>
2024-11-23 14:10:13 +0800

    doc: generate docs for packer

--- scripts/docs.rb
+++ scripts/docs.rb
@@ -1,26 +1,62 @@
 #!/usr/bin/env ruby
 
-class FuncDoc
-  attr_accessor :comment
+class BaseDoc
+  attr_accessor :comment, :signature
 
-  def initialize
+  def initialize(comment)
     # documentation comment above the function
-    @comment = ""
-
-    # full signature of the function
+    @comment = comment
     @signature = ""
   end
 
-  def signature=(signature)
-    @signature = signature.strip
-  end
-
-  def name
-    @signature.match(/ (\w+)\(/)[1]
+  def comment_as_markdown
+    # TODO: apply fancy markdown links to comment
+    #       for example all types and functions in comment
+    #       that are known ddnet_protocol types should be
+    #       markdown links to their documentation entry
+    @comment
   end
 
   def valid?
     !@comment.empty? && !@signature.empty?
+  end
+end
+
+class StructDoc < BaseDoc
+  def initialize(comment)
+    super
+  end
+
+  def name
+    @signature.split("\n").last.match(/ (\w+)/)[1]
+  end
+
+  def to_markdown
+    return "invalid" unless valid?
+
+    markdown = ""
+    markdown << "# #{name}\n\n"
+    markdown << "## Syntax\n\n"
+    markdown << "```C\n"
+    markdown << @signature
+    markdown << "```\n\n"
+    markdown << comment_as_markdown
+    markdown
+  end
+end
+
+class FuncDoc < BaseDoc
+  attr_accessor :comment
+
+  def initialize(comment, signature)
+    super(comment)
+
+    # full signature of the function
+    @signature = signature.strip
+  end
+
+  def name
+    @signature.match(/ \*?(\w+)\(/)[1]
   end
 
   def to_markdown
@@ -33,47 +69,77 @@ class FuncDoc
     markdown << @signature
     markdown << "\n"
     markdown << "```\n\n"
-    # TODO: apply fancy markdown links to comment
-    #       for example all types and functions in comment
-    #       that are known ddnet_protocol types should be
-    #       markdown links to their documentation entry
-    markdown << "#{@comment}"
+    markdown << comment_as_markdown
     markdown
   end
 end
 
+def is_func?(line)
+  !line.match(/(const )?(u?int(32|8)_t|Unpacker) \*?\w+\(/).nil?
+end
+
+def is_struct?(line)
+  line.strip == "typedef struct {"
+end
+
 # @param filepath String
-# @return FuncDoc[]
-def parse_func_docs(filepath)
-  func_docs = []
-  func_doc = FuncDoc.new
+# @return (FuncDoc|StructDoc)[]
+def parse_docs(filepath)
+  docs = []
+  comment = ""
+  struct = nil
 
   IO.foreach(filepath) do |line|
     next if line.strip.empty?
     next if line[0] == '#'
 
+    # seek till struct end if we got one
+    if struct
+      struct.signature += line
+      if line.start_with? "} "
+        docs << struct
+        struct = nil
+      end
+      next
+    end
+
     if line.start_with? "// "
-      func_doc.comment += line[3..]
+      comment += line[3..]
       next
     end
     if line.strip == "//"
-      func_doc.comment += "\n"
+      comment += "\n"
       next
     end
 
     # if its not a comment or a filtered line
-    # we assume it has to be the function signature
-    func_doc.signature = line
-    func_docs << func_doc
+    # we assume we hit the C type that is being document
+    # so either a struct or function signature
+    if is_func? line
+      docs << FuncDoc.new(comment, line)
+    elsif is_struct? line
+      # we have to seek to the end of the struct before
+      # we can store the instance
+      struct = StructDoc.new(comment)
+      struct.signature += line
+    else
+      raise "Expected function or struct got unknown: #{line}"
+    end
 
-    # create a empty doc obj for the next function
-    func_doc = FuncDoc.new
+    # create a empty doc comment for next entry
+    comment = ""
   end
-  func_docs
+  docs
 end
 
-parse_func_docs("src/huffman.h").each do |func_doc|
-  puts func_doc.to_markdown
-  puts ""
+def header_to_markdown(header_path, markdown_path)
+  markdown = parse_docs(header_path).map do |doc|
+    doc.to_markdown + "\n"
+  end.inject(:+)
+  File.write(markdown_path, markdown)
+end
+
+%w(packer huffman).each do |component|
+  header_to_markdown("src/#{component}.h", "docs/#{component}.md")
 end
 

67ae71957cd31960569ad59c4733f07f96a0b6eb
ChillerDragon <chillerdragon@gmail.com>
2024-11-29 15:54:14 +0800

    ci: check for stdlib includes (Closed #32)
    Yoinked from https://github.com/ddnet/ddnet/blob/787f85c25de77d6fd7abf6f7327ce0089a3fd4d3/scripts/check_standard_headers.sh
    And adjusted for our use case

---
+++ scripts/check_standard_headers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# List of C headers
+c_headers=(
+	assert.h complex.h ctype.h errno.h fenv.h float.h inttypes.h iso646.h limits.h locale.h math.h setjmp.h signal.h stdarg.h stdbool.h stddef.h stdint.h stdio.h stdlib.h string.h tgmath.h time.h wchar.h wctype.h
+)
+
+# Create regex dynamically from the array to match any C header
+c_headers_regex=$(printf "%s" "${c_headers[*]}" | tr ' ' '|')
+
+# Find all C source and header files
+files=$(find ./src -type f \( -name '*.c' -o -name '*.h' \) ! -path "./include/ddnet_protocol/common.h")
+
+error_found=0
+
+# Check each source file for stdlib headers
+for file in $files; do
+	if grep -E "#include\s+<($c_headers_regex)>" "$file" > /dev/null; then
+		echo "Error: '$file' includes stdlib header '${c_headers[i]}'. Include 'ddnet_protocol/common.h' instead."
+		error_found=1
+	fi
+done
+
+if [ $error_found -eq 1 ]; then
+	exit 1
+fi
+
+echo "Success: No standard C headers are used."

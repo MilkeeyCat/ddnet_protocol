06f04f060e2d662c15babdfb3467170b09dbc210
ChillerDragon <chillerdragon@gmail.com>
2024-12-07 10:45:47 +0800

    test: add packet header test

---
+++ test/packet_header.cc
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <ddnet_protocol/packet.h>
+}
+
+TEST(PacketHeader, NormalNoFlags) {
+	uint8_t bytes[] = {0x00, 0x06, 0x02};
+	PacketHeader header = decode_packet_header(bytes);
+	EXPECT_EQ(header.flags, 0);
+	EXPECT_EQ(header.ack, 6);
+	EXPECT_EQ(header.num_chunks, 2);
+}
+

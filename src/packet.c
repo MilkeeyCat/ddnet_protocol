9e97068d858131223e9a54cecf0de054d2fe4657
MilkeeyCat <milkeeycat@gmail.com>
2024-12-10 22:07:53 +0200

    refactor: more efficient way of working with chunks

--- src/packet.c
+++ src/packet.c
@@ -11,32 +11,51 @@ PacketHeader decode_packet_header(uint8_t *buf) {
 	};
 }
 
-Packet *decode(uint8_t *buf, size_t len, Error *err) {
+typedef struct {
+	Chunk *chunks;
+	size_t len;
+} Context;
+
+static void on_chunk(void *ctx, Chunk *chunk) {
+	Context *context = ctx;
+
+	memcpy(&context->chunks[context->len++], chunk, sizeof(Chunk));
+}
+
+Packet decode(uint8_t *buf, size_t len, Error *err) {
+	Packet packet;
+
 	if(len < PACKET_HEADER_SIZE || len > MAX_PACKET_SIZE) {
 		if(err) {
 			*err = ERR_INVALID_PACKET;
 		}
 
-		return NULL;
+		return packet;
 	}
 
-	Packet *packet = malloc(sizeof(Packet));
-	memset(packet, 0, sizeof(*packet));
-	packet->header = decode_packet_header(buf);
+	packet.header = decode_packet_header(buf);
 
-	if(packet->header.flags & PACKET_FLAG_CONTROL) {
-		packet->kind = PACKET_CONTROL;
-		packet->control = decode_control(&buf[3], len - 3, &packet->header, err);
+	if(packet.header.flags & PACKET_FLAG_CONTROL) {
+		packet.kind = PACKET_CONTROL;
+		packet.control = decode_control(&buf[3], len - 3, &packet.header, err);
 	} else {
-		packet->kind = PACKET_NORMAL;
-		Error chunk_error = fetch_chunks(&buf[3], len - 3, packet);
+		packet.kind = PACKET_NORMAL;
+		Context ctx = {
+			.chunks = malloc(sizeof(Chunk) * packet.header.num_chunks),
+			.len = 0,
+		};
+		Error chunk_error = fetch_chunks(&buf[3], len - 3, &packet.header, on_chunk, &ctx);
+
 		if(chunk_error != ERR_NONE) {
 			if(err) {
 				*err = chunk_error;
 			}
-			free_packet(packet);
-			return NULL;
+
+			return packet;
 		}
+
+		packet.chunks.data = ctx.chunks;
+		packet.chunks.len = ctx.len;
 	}
 
 	return packet;
@@ -44,10 +63,8 @@ Packet *decode(uint8_t *buf, size_t len, Error *err) {
 
 Error free_packet(Packet *packet) {
 	if(packet->kind == PACKET_NORMAL) {
-		for(size_t i = 0; i < MAX_CHUNKS; i++) {
-			free(packet->chunks[i].msg.unused);
-		}
+		free(packet->chunks.data);
 	}
-	free(packet);
+
 	return ERR_NONE;
 }

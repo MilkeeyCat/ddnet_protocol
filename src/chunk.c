83fd90aebf9d32f76ef2cff0fd4abfa5ccef959f
ChillerDragon <chillerdragon@gmail.com>
2024-12-20 16:18:12 +0800

    feat: chunk header encoding and flags refactor
    Keeping the chunk flags vital and resend closer to the network protocol.
    This removes the need for shifting the flags around and makes setting
    them by hand smoother.
    
    Replaced some hex with decimal values in the test because they represent
    numbers not raw data.

--- src/chunk.c
+++ src/chunk.c
@@ -4,8 +4,8 @@ ChunkHeader decode_chunk_header(uint8_t **buf_ptr) {
 	ChunkHeader header;
 	uint8_t *buf = *buf_ptr;
 
-	header.flags = (buf[0] >> 6) & 3;
-	header.size = ((buf[0] & 0x3f) << 4) | (buf[1] & ((1 << 4) - 1));
+	header.flags = buf[0] & 0b11000000;
+	header.size = ((buf[0] & 0b00111111) << 4) | (buf[1] & ((1 << 4) - 1));
 	header.sequence = 0;
 
 	if(header.flags & CHUNK_FLAG_VITAL) {
@@ -17,4 +17,15 @@ ChunkHeader decode_chunk_header(uint8_t **buf_ptr) {
 	}
 
 	return header;
+}
+
+uint8_t *encode_chunk_header(const ChunkHeader *header, uint8_t *buf) {
+	buf[0] = header->flags & 0b11000000 | ((header->size >> 4) & 0b00111111);
+	buf[1] = (header->size & 0b00001111);
+	if(header->flags & CHUNK_FLAG_VITAL) {
+		buf[1] |= (header->sequence >> 2) & 0b11110000;
+		buf[2] = header->sequence & 0xff;
+		return buf + 3;
+	}
+	return buf + 2;
 }

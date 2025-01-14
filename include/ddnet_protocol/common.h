35cc3df4e866af4060237c19f0f8641b91327f5e
MilkeeyCat <milkeeycat@gmail.com>
2025-01-14 20:15:50 +0200

    refactor: include linux header files directly `BLOAT` macro doesn't work because `true` and `false` enum values are redeclared(first time in `linux/types.h`, second time in `common.h`)

--- include/ddnet_protocol/common.h
+++ include/ddnet_protocol/common.h
@@ -1,44 +1,30 @@
 #pragma once
 
+#define KERNEL_SPACE 1
+#define USER_SPACE 2
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef BLOAT
-#include <assert.h>
+#if CODE_SPACE == KERNEL_SPACE
+// Non Linux users are in shambles
+#include <linux/string.h>
+#include <linux/types.h>
+
+extern void *malloc(size_t size);
+extern void free(void *ptr);
+#elif CODE_SPACE == USER_SPACE
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#elif !defined(CODE_SPACE)
+#error \
+	"CODE_SPACE is not defined. Define it to `KERNEL_SPACE` or `USER_SPACE`."
 #else
-#ifndef assert
-#define assert
-#endif
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
-#ifndef _MSC_VER
-typedef long unsigned int size_t;
-#endif
-#ifndef __cplusplus
-typedef enum { false,
-	true } bool;
-#endif
-
-extern void *memset(void *s, int c, size_t n);
-extern void *malloc(size_t size);
-extern void free(void *ptr);
-extern char *strncpy(char *dest, const char *src, size_t len);
-extern void *memcpy(void *dest, const void *src, size_t n);
-#ifndef strlen
-extern size_t strlen(const char *str);
-#endif
-#endif
-
-#ifndef NULL
-#define NULL ((void *)0)
+#error \
+	"CODE_SPACE has invalid value, it must be `KERNEL_SPACE` or `USER_SPACE`."
 #endif
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))

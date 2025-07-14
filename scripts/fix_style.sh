8215fa89d2b45a89f6bb97884fe69b0db08d3035
ChillerDragon <chillerdragon@gmail.com>
2025-07-14 13:50:33 +0200

    feat: example client

--- scripts/fix_style.sh
+++ scripts/fix_style.sh
@@ -5,7 +5,7 @@ clang_arg="${1:--i}"
 while read -r src_file
 do
 	clang-format -Werror "$clang_arg" "$src_file" || exit 1
-done < <(find ./src ./include ./test \( -name '*.h' -o -name '*.c' -o -name '*.cc' \))
+done < <(find ./src ./include ./test ./examples \( -name '*.h' -o -name '*.c' -o -name '*.cc' \))
 
 if grep -rnEo '0x[A-Z0-9]+' src/ | grep -E '0x.*[A-Z]'
 then

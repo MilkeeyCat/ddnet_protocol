cfc72e2038b530e8afbfb4dd07d5ed5ffcf7d013
ChillerDragon <chillerdragon@gmail.com>
2025-07-31 08:57:09 +0200

    feat: add some game messages

--- include/ddnet_protocol/msg_game.h
+++ include/ddnet_protocol/msg_game.h
@@ -6,6 +6,36 @@ extern "C" {
 
 #include "common.h"
 
+// message of the day
+// sent by the server
+// and displayed in the middle of the screen
+// with transparent background on the client side
+typedef struct {
+	const char *message;
+} DDNetMsgSvMotd;
+
+// sent by the server
+// and displayed in the middle of the screen as white text
+typedef struct {
+	const char *message;
+} DDNetMsgSvBroadcast;
+
+// sent by the server
+typedef struct {
+	// 0 - public chat
+	// 1 - team chat
+	// 2 - whisper send (ddnet extension)
+	// 3 - whisper receive (ddnet extension)
+	int32_t team;
+
+	// client id of the message author
+	// can be -1 if the message was sent by the server
+	int32_t client_id;
+
+	// chat message
+	const char *message;
+} DDNetMsgSvChat;
+
 typedef struct {
 	const char *name;
 	const char *clan;

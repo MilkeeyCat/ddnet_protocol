c7c1ca0f57ddf2ba67638c5acfa6612037822327
ChillerDragon <ChillerDragon@gmail.com>
2025-12-25 17:22:08 +0100

    feat: ints to string converter for snap unpacking

---
+++ docs/int_string.md
@@ -0,0 +1,35 @@
+# ddnet_str_length
+
+## Syntax
+
+```C
+size_t ddnet_str_length(const char *str);
+```
+
+Get the length of a string in bytes.
+
+# ddnet_ints_to_str
+
+## Syntax
+
+```C
+DDNetError ddnet_ints_to_str(const uint32_t *ints, size_t num_ints, char *str, size_t str_len);
+```
+
+Convert list of integers into a string.
+Used by the snapshot which technically only consists of integers, but also holds strings.
+
+# ddnet_str_to_ints
+
+## Syntax
+
+```C
+DDNetError ddnet_str_to_ints(uint32_t *ints, size_t num_ints, const char *str);
+```
+
+Convert string to a list of integers.
+Used by the snapshot which technically only consists of integers, but also holds strings.
+If the string is too big and does not fit into `num_ints` amount of integers the function
+returns an error. If the string is so short that it does not need `num_ints` integers
+to be represented it still fills all the integers with padding.
+
---
+++ src/int_string.c
@@ -0,0 +1,54 @@
+#include <ddnet_protocol/errors.h>
+#include <ddnet_protocol/int_string.h>
+
+size_t ddnet_str_length(const char *str) {
+	size_t len = 0;
+	while(*(str++)) {
+		len++;
+	}
+	return len;
+}
+
+DDNetError ddnet_ints_to_str(const uint32_t *ints, size_t num_ints, char *str, size_t str_len) {
+	if(str_len < num_ints * sizeof(uint32_t)) {
+		return DDNET_ERR_OUTPUT_VAR_TOO_SMALL;
+	}
+
+	// unpack string without validation
+	size_t str_index = 0;
+	for(size_t i = 0; i < num_ints; i++) {
+		const uint32_t current_int = ints[i];
+		str[str_index++] = (char)(((current_int >> 24) & 0xff) - 128);
+		str[str_index++] = (char)(((current_int >> 16) & 0xff) - 128);
+		str[str_index++] = (char)(((current_int >> 8) & 0xff) - 128);
+		str[str_index++] = (char)((current_int & 0xff) - 128);
+	}
+	// ensure null-termination
+	str[str_index - 1] = '\0';
+
+	// TODO: check valid utf8
+	return DDNET_ERR_NONE;
+}
+
+DDNetError ddnet_str_to_ints(uint32_t *ints, size_t num_ints, const char *str) {
+	const size_t str_len = ddnet_str_length(str);
+	if(str_len > num_ints * sizeof(uint32_t)) {
+		return DDNET_ERR_OUTPUT_VAR_TOO_SMALL;
+	}
+
+	for(size_t i = 0; i < num_ints; i++) {
+		// copy to temporary buffer to ensure we don't read past the end of the input string
+		char buf[sizeof(uint32_t)] = {0, 0, 0, 0};
+		for(size_t buf_idx = 0; buf_idx < sizeof(uint32_t) && (i * sizeof(uint32_t)) + buf_idx < str_len; buf_idx++) {
+			buf[buf_idx] = str[(i * sizeof(uint32_t)) + buf_idx];
+		}
+		ints[i] = ((uint32_t)(buf[0] + 128) << 24) |
+			  ((buf[1] + 128) << 16) |
+			  ((buf[2] + 128) << 8) |
+			  (buf[3] + 128);
+	}
+	// last byte is always zero and unused in this format
+	ints[num_ints - 1] &= 0xffffff00;
+
+	return DDNET_ERR_NONE;
+}
